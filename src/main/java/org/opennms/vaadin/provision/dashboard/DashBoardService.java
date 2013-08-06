fa99bc017a2e2cbdb177a331e173bbb544780373
Antonio <rssntn67@yahoo.it>
2013-08-07 00:32:49 +0200

    Added properties file
    You can set the url to be connected as the db access info

--- src/main/java/org/opennms/vaadin/provision/dashboard/DashBoardService.java
+++ src/main/java/org/opennms/vaadin/provision/dashboard/DashBoardService.java
@@ -1,9 +1,13 @@
 package org.opennms.vaadin.provision.dashboard;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -34,6 +38,13 @@ public class DashboardService {
     
 	protected static final String LABEL = "nodeLabel";
 
+	protected static final String PROPERTIES_FILE_PATH = "/etc/tomcat7/provision-dashboard.properties";
+	protected static final String PROPERTIES_URLS_KEY = "urls";
+	protected static final String PROPERTIES_URL_ROOT_KEY = "url";
+	protected static final String PROPERTIES_DB_URL_KEY = "db_url";
+	protected static final String PROPERTIES_DB_USER_KEY = "db_username";
+	protected static final String PROPERTIES_DB_PASS_KEY = "db_password";
+
 	protected String[] URL_LIST = new String[] {
 		"http://demo.arsinfo.it/opennms/rest",
 		"http://localhost:8980/opennms/rest"
@@ -52,6 +63,8 @@ public class DashboardService {
 	
 	private SQLContainer m_snmpProfiles;
 	private SQLContainer m_backupProfiles;
+	private Properties m_configuration = new Properties();
+
 	private BeanContainer<String, TrentinoNetworkRequisitionNode> m_requisitionContainer = new BeanContainer<String, TrentinoNetworkRequisitionNode>(TrentinoNetworkRequisitionNode.class);
 
 	public BeanContainer<String, TrentinoNetworkRequisitionNode> getRequisitionContainer() {
@@ -70,6 +83,15 @@ public class DashboardService {
     	m_nodeService = new JerseyNodesService();
     	m_snmpInfoService = new JerseySnmpInfoService();
     	m_foreignSourceService = new JerseyProvisionForeignSourceService();
+    	File file = new File(PROPERTIES_FILE_PATH);
+    	if (file.exists() && file.isFile()) {
+    		try {
+    			m_configuration.load(new FileInputStream(file));
+    		} catch (IOException ex) {
+    			System.out.println("No configuration file found using default");
+    		}
+  
+    	}
     }
     
 	public JerseyClientImpl getJerseyClient() {
@@ -114,7 +136,7 @@ public class DashboardService {
 		setJerseyClient(
 				new JerseyClientImpl(url,username,password));
 		m_snmpInfoService.get("127.0.0.1");
-		m_pool = new SimpleJDBCConnectionPool("org.postgresql.Driver", "jdbc:postgresql://172.25.200.36:5432/tnnet", "isi_writer", "Oof6Eezu");
+		m_pool = new SimpleJDBCConnectionPool("org.postgresql.Driver", getDbUrl(), getDbUsername(), getDbPassword());
 		loadSnmpProfiles();
 	}
 
@@ -231,9 +253,41 @@ public class DashboardService {
 	}
 
 	public String[] getUrls() {
-		return URL_LIST;
+		List<String> urls = new ArrayList<String>();
+		if (m_configuration.getProperty(PROPERTIES_URLS_KEY) != null ) {
+			for (String urlKey: m_configuration.getProperty(PROPERTIES_URLS_KEY).split(",")) {
+				String key = PROPERTIES_URL_ROOT_KEY+"."+urlKey;
+				if (m_configuration.getProperty(key) != null)
+					urls.add(m_configuration.getProperty(key));
+			}
+		}
+		if (urls.size() > 0)
+			return urls.toArray(new String[urls.size()]);
+		else
+			return URL_LIST;
 	}
 
+	public String getDbUrl() {
+		if (m_configuration.getProperty(PROPERTIES_DB_URL_KEY) != null ) 
+			return m_configuration.getProperty(PROPERTIES_DB_URL_KEY);
+		else 
+			return "jdbc:postgresql://172.25.200.36:5432/tnnet";
+	}
+	
+	public String getDbUsername() {
+		if (m_configuration.getProperty(PROPERTIES_DB_USER_KEY) != null ) 
+			return m_configuration.getProperty(PROPERTIES_DB_USER_KEY);
+		else 
+			return "isi_writer";
+	}
+	
+	public String getDbPassword() {
+		if (m_configuration.getProperty(PROPERTIES_DB_PASS_KEY) != null ) 
+			return m_configuration.getProperty(PROPERTIES_DB_PASS_KEY);
+		else 
+			return "Oof6Eezu";
+		
+	}
 	public List<String> getIpAddresses(String foreignSource,String nodelabel) {
 	   	List<String> ipaddresses = new ArrayList<String>();
 		MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
