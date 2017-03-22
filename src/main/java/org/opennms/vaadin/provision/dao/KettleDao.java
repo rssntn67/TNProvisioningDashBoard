0592456f10d2a331cdd471fafc8b5bda4c71965a
Antonio <rssntn67@yahoo.it>
2017-03-22 12:24:42 +0100

    Added KettleDao

---
+++ src/main/java/org/opennms/vaadin/provision/dao/KettleDao.java
@@ -0,0 +1,58 @@
+package org.opennms.vaadin.provision.dao;
+
+import org.opennms.rest.client.JerseyClientImpl;
+import org.opennms.rest.client.JerseyKettleService;
+import org.opennms.rest.client.model.KettleJobStatus;
+import org.opennms.rest.client.model.KettleRunJob;
+
+public class KettleDao {
+
+	private JerseyClientImpl m_jerseyClient;
+    
+	private JerseyKettleService m_kettleService;
+
+	public KettleDao() {
+		m_kettleService = new JerseyKettleService();
+	}
+	
+	public JerseyClientImpl getJerseyClient() {
+		return m_jerseyClient;
+	}
+
+	public void setJerseyClient(JerseyClientImpl jerseyClient) {
+		m_jerseyClient = jerseyClient;
+	    m_kettleService.setJerseyClient(m_jerseyClient);
+	}
+
+	public KettleRunJob runJob() {
+		return m_kettleService.runJob();
+	}
+	
+	public KettleJobStatus jobStatus(KettleRunJob job) {
+		return m_kettleService.jobStatus(job);
+	}
+	
+	public boolean isFinished(KettleJobStatus status) {
+		return "Finished".equals(status.getStatusDescr());
+	}
+
+	public boolean isRunning(KettleJobStatus status) {
+		return "Running".equals(status.getStatusDescr());
+	}
+	
+	public boolean isCompleted(KettleJobStatus status) {
+		if (status.getResult() == null )
+			return false;
+		if (!"Y".equals(status.getResult().getResult()))
+			return false;
+		if (status.getResult().getExitStatus() == null )
+			return false;
+		try {
+			if ( 0 != Integer.valueOf(status.getResult().getExitStatus()).intValue() )
+				return false;
+		} catch (Exception e) {
+			return false;
+		}
+		return "N".equals( status.getResult().getIsStopped());
+	}
+}
