fec34d9c27bdc2120a40aed5455759acdda73e86
Antonio <rssntn67@yahoo.it>
2017-09-06 11:23:19 +0200

    Fixed NPE for thread indipndence

--- src/main/java/org/opennms/vaadin/provision/dashboard/FastTab.java
+++ src/main/java/org/opennms/vaadin/provision/dashboard/FastTab.java
@@ -278,18 +278,18 @@ public class FastTab extends DashboardTab {
 			        m_progress.setIndeterminate(true);
 			        m_progress.setVisible(true);
 			        m_progress.setEnabled(true);
-			        startJob();
 				}
 			});
+	        startJob();
 		}
 
 		@Override
 		public void afterJob() {
+			endJob();
 			UI.getCurrent().access(new Runnable() {
 
 				@Override
 				public void run() {
-					endJob();
 					m_progress.setValue(new Float(0.0));
 					m_progress.setEnabled(false);
 					m_progress.setVisible(false);
